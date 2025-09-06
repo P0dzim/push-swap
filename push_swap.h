38d3ac0aa0f486ff651154867939babdfd85b814
P0dzim <vitor.200483@gmail.com>
2025-09-06 15:35:46 -0300

    create header and gitignore

---
+++ push_swap.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vitosant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/06 11:50:08 by vitosant          #+#    #+#             */
+/*   Updated: 2025/09/06 11:50:11 by vitosant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# define INT_EXPLOSION 3000000000
+# define INT_HIGH	2147483647 
+# define INT_LOW	-2147483648
+
+# include "libft/libft.h"
+
+typedef struct s_stack
+{
+	int	value;
+	int	index;
+	int	pos;
+	int	target_pos;
+	int	cost_a;
+	int	cost_b;
+}		t_stack;
+
+long	ft_atol_checker(const char *nptr);
+void	create_stack(char **tab);
+int	fill_stack(t_stack *stack, char **tab);
+
+#endif
