861454f187acb0a627194a4d0a08b9862302eba5
P0dzim <vitor.200483@gmail.com>
2025-09-06 15:31:53 -0300

    creat atol_checker

---
+++ ft_atol_checker.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/12 09:31:14 by vitosant          #+#    #+#             */
+/*   Updated: 2025/07/18 09:02:17 by vitosant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+long	ft_atol_checker(const char *nptr)
+{
+	long	i;
+	long	sign;
+	long	num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9' && i <= 11)
+	{
+		num = (num * 10) + (nptr[i] - '0');
+		i++;
+	}
+	if (nptr[i] != '\0')
+		return (INT_EXPLOSION);
+	return (num * sign);
+}
